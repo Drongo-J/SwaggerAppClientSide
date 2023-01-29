5e13bb910cf7dedea1154bb16252fe7cc09ed4c4
Elvin Jamalzade <camalzadeelvin@itstep.edu.az>
2023-01-29 16:18:46 +0400

    Add project files.

---
+++ ClientSwaggerApp/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClientSwaggerApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly Socket ClientSocket = new Socket
+            (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        private const int PORT = 27001;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ConnectToServer();
+            RequestLoop();
+        }
+
+
+        private void ConnectToServer()
+        {
+            int attempts = 0;
+
+            while (!ClientSocket.Connected)
+            {
+                try
+                {
+                    attempts++;
+                    // Change IPAddress.Loopback to a remote IP to connect to a remote host.
+                    ClientSocket.Connect(IPAddress.Parse("10.2.13.15"), PORT);
+                }
+                catch (SocketException)
+                {
+                }
+            }
+
+            MessageBox.Show("Connected");
+        }
+
+
+        private void RequestLoop()
+        {
+
+            var receiver = Task.Run(() =>
+            {
+
+                while (true)
+                {
+                    ReceiveResponse();
+
+                }
+            });
+
+            //Task.WaitAll(receiver);
+        }
+
+        /// <summary>
+        /// Sends a string to the server with ASCII encoding.
+        /// </summary>
+        private void SendString(string text)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(text);
+            ClientSocket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+        }
+
+        private void ReceiveResponse()
+        {
+            var buffer = new byte[2048];
+            int received = ClientSocket.Receive(buffer, SocketFlags.None);
+            if (received == 0) return;
+            var data = new byte[received];
+            Array.Copy(buffer, data, received);
+            string text = Encoding.ASCII.GetString(data);
+            App.Current.Dispatcher.Invoke(() =>
+            {
+                ResponseTxtb.Text = text;
+            });
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            SendString(requestTxtb.Text);
+        }
+    }
+}
