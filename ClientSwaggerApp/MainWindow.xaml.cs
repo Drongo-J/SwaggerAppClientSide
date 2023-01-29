eb928e7b2c1b9b14575e1afda9c30f5a600ee24d
Elvin Jamalzade <camalzadeelvin@itstep.edu.az>
2023-01-29 17:30:31 +0400

    done

--- ClientSwaggerApp/MainWindow.xaml.cs
+++ ClientSwaggerApp/MainWindow.xaml.cs
@@ -59,8 +59,91 @@ namespace ClientSwaggerApp
             }
 
             MessageBox.Show("Connected");
+
+            var buffer = new byte[2048];
+            int received = ClientSocket.Receive(buffer, SocketFlags.None);
+            if (received == 0) return;
+            var data = new byte[received];
+            Array.Copy(buffer, data, received);
+            string text = Encoding.ASCII.GetString(data);
+            App.Current.Dispatcher.Invoke(() =>
+            {
+                ParseForView(text);
+            });
         }
 
+        private void ParseForView(string text)
+        {
+            App.Current.Dispatcher.Invoke(() =>
+            {
+                var commands = text.Split('\n');
+                var result = commands.ToList();
+                result.Remove("");
+                foreach (var item in result)
+                {
+                    Button button = new Button();
+                    button.FontSize = 22;
+                    button.Margin = new Thickness(0, 10, 0, 0);
+                    button.Content = item;
+                    button.Click += Button_Click1;
+                    CommandsStackPanel.Children.Add(button);
+                }
+            });
+        }
+
+        TextBox textBox = new TextBox();
+        public string SelectedCommand { get; set; }
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button bt)
+            {
+                var content = bt.Content.ToString();
+                var result = content.Remove(content.Length - 1, 1);
+                SelectedCommand = result;
+                var splitResult = result.Split('\\');
+                if (splitResult.Length > 2)
+                {
+                    textBox.Width = 200;
+                    textBox.Height = 60;
+                    textBox.Text = "*" + splitResult[2];
+                    textBox.FontSize = 22;
+                    if (paramsStackPanel.Children.Count > 3)
+                    {
+                        paramsStackPanel.Children.RemoveAt(3);
+                        paramsStackPanel.Children.RemoveAt(3);
+                    }
+                    paramsStackPanel.Children.Add(textBox);
+
+                    Button button = new Button();
+                    button.FontSize = 22;
+                    button.Margin = new Thickness(0, 10, 0, 0);
+                    button.Content = "Execute";
+                    button.Click += Button_Click2; ;
+                    paramsStackPanel.Children.Add(button);
+
+                }
+                else
+                {
+                    if (paramsStackPanel.Children.Count > 3)
+                    {
+                        paramsStackPanel.Children.RemoveAt(3);
+                        paramsStackPanel.Children.RemoveAt(3);
+                    }
+                    SendString(result);
+                }
+            }
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            var result = SelectedCommand.Split('\\');
+            var resultText = result[0] + "\\"+result[1] + "\\" + textBox.Text;
+            if (SelectedCommand.Contains("json"))
+            {
+                resultText= result[0] + "\\" + result[1] + " " + textBox.Text;
+            }
+            SendString(resultText);
+        }
 
         private void RequestLoop()
         {
